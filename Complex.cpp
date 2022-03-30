7357f333b16bf9b0a408aa5fcb5e9bb1b0856a8d
rigz <49341977+r1gz@users.noreply.github.com>
2022-03-30 22:41:36 +0300

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,5 +1,5 @@
-﻿
 #include <iostream>
+#include <string>
 #include <locale>
 #include <Windows.h>
 
@@ -11,12 +11,65 @@ void localize() {
 	SetConsoleOutputCP(1251);
 }
 
+void string_split_smart(string str, string razd, string* &result, int& count) {
+	for (int i = 0; i < str.length(); i++) {
+		int l = razd.find(str[i]);
+		if (l < 0 || l > razd.length()) {
+			result[count] += str[i];
+		}
+		else {
+			if (str[i] == ' ') {
+				if (result[count] != "") {
+					result[++count] = "";
+				}
+			}
+			else {
+ 				result[++count] = str[i];
+			}
+		}
+	}
+}
+
 class Complex {
 public:
 	Complex(int _real = 0, int _im = 0) {
 		real = _real;
 		im = _im;
-		mod = sqrt(real^2 + im^2);
+
+		real_f = (double)real;
+		im_f = (double)im;
+
+		mod = sqrt(real_f * real_f + im_f * im_f);
+	}
+
+	Complex(double _real, double _im = 0.0) {
+		real_f = _real;
+		im_f = _im;
+
+		real = (int)real_f;
+		im = (int)im_f;
+
+		mod = sqrt(real_f * real_f + im_f * im_f);
+	}
+
+	Complex(int _real, double _im = 0.0) {
+		real_f = (double)_real;
+		im_f = _im;
+
+		real = _real;
+		im = (int)im_f;
+
+		mod = sqrt(real_f * real_f + im_f * im_f);
+	}
+
+	Complex(double _real, int _im = 0) {
+		real_f = _real;
+		im_f = (double)_im;
+
+		real = (int)real_f;
+		im = _im;
+
+		mod = sqrt(real_f * real_f + im_f * im_f);
 	}
 
 	int GetReal() {
@@ -28,87 +81,120 @@ public:
 	}
 
 	double GetMod() {
-		mod = sqrt(real ^ 2 + im ^ 2);
+		mod = sqrt(real_f * real_f + im_f * im_f);
 		return this->mod;
 	}
 
 	void SetReal(int rl) {
 		real = rl;
-		mod = sqrt(real ^ 2 + im ^ 2);
+		real_f = (double)real;
+		mod = sqrt(real_f * real_f + im_f * im_f);
 	}
 
 	void SetIm(int iml) {
 		im = iml;
-		mod = sqrt(real ^ 2 + im ^ 2);
+		im_f = (double)im;
+		mod = sqrt(real_f * real_f + im_f * im_f);
 	}
+
+	void SetReal(double rl) {
+		real_f = rl;
+		real = (int)real_f;
+		mod = sqrt(real_f * real_f + im_f * im_f);
+	}
+
+	void SetIm(double iml) {
+		im_f = iml;
+		im = (int)im_f;
+		mod = sqrt(real_f * real_f + im_f * im_f);
+	}
+
+	~Complex(void) {
+		real = 0;
+		im = 0;
+		real_f = 0.0;
+		im_f = 0.0;
+		mod = 0;
+	};
 
 	Complex operator+(Complex comp) {
 		return Complex(real + comp.real, im + comp.im);
 	}
 
 	Complex operator-(Complex comp) {
-		return Complex(real - comp.real, im - comp.im);
+		return Complex(real + comp.real, im + comp.im);
 	}
 
 	Complex operator*(Complex comp) {
-		return Complex(real*comp.real - im*comp.im, real*comp.im + comp.real*im);
+		return Complex(real * comp.real - im * comp.im, real * comp.im + comp.real * im);
 	}
 
 	Complex operator/(Complex comp) {
 		double sum_sqrt = comp.GetMod();
 
-		try {
-			if (sum_sqrt == 0.f) {
-				throw sum_sqrt;
-			}
-			else {
-				int new_real = (real * comp.real + im * comp.im) / sum_sqrt;
-				int new_im = (real * comp.im - comp.im * im) / sum_sqrt;
-
-				return Complex(new_real, new_im);
-			}
-		}
-		catch (int myNum) {
+		if (sum_sqrt == 0.f) {
 			cout << "Деление на 0 невозможно, попробуйте ввести другое число" << endl;
 			return Complex();
 		}
+		else {
+			return Complex((real_f * comp.real_f + im_f * comp.im_f) / sum_sqrt, (real_f * comp.im_f - comp.im_f * im_f) / sum_sqrt);
+		}
+
 	}
 
-	friend ostream& operator<<(ostream &out, const Complex &comp) {
-		if (comp.real != 0) {
-			if (comp.im > 1) {
-				out << comp.real << "+i" << comp.im;
+	friend ostream& operator<<(ostream& out, const Complex& comp) {
+
+		string reals;
+		string ims;
+
+		if (ceil(comp.real_f) > comp.real) {
+			reals = to_string(comp.real_f);
+		}
+		else {
+			reals = to_string(comp.real);
+		}
+
+		if (ceil(comp.im_f) > comp.im) {
+			ims = to_string(comp.im_f);
+		}
+		else {
+			ims = to_string(comp.im);
+		}
+
+		if (comp.real_f != 0.0) {
+			if (comp.im_f > 1.0) {
+				out << reals << "+" << ims << "i";
 			}
-			else if (comp.im < -1) {
-				out << comp.real << "-i" << abs(comp.im);
+			else if (comp.im_f < -1.0) {
+				out << reals << ims << "i";
 			}
 			else {
-				if (comp.im > 0) {
-					out << comp.real << "+i";
+				if (comp.im_f > 0.0) {
+					out << reals << "+i";
 				}
-				else if (comp.im < 0) {
-					out << comp.real << "-i";
-				} else {
-					out << comp.real;
+				else if (comp.im_f < 0.0) {
+					out << reals << "-i";
+				}
+				else {
+					out << reals;
 				}
 			}
 		}
 		else {
-			if (comp.im > 1) {
-				out << "i" << comp.im;
-			}
-			else if (comp.im < -1) {
-				out << "-i" << abs(comp.im);
-			}
-			else {
-				if (comp.im > 0) {
+
+			if (abs(comp.im_f) == 1.0) {
+				if (comp.im_f > 0.0) {
 					out << "i";
-				}
-				else if (comp.im < 0) {
+				} else {
 					out << "-i";
 				}
-				else {
+			}
+			else {
+				if (comp.im_f == 0.0) {
 					out << 0;
+				}
+				else {
+					out << ims << "i";
 				}
 			}
 		}
@@ -117,33 +203,86 @@ public:
 	}
 
 	friend istream& operator>>(istream& in, Complex& comp) {
-		in >> comp.real;
-		in >> comp.im;
+		comp.SetReal(0);
+		comp.SetIm(0);
 
-		comp.mod = sqrt(comp.real ^ 2 + comp.im ^ 2);
+		string* w = new string[3];
+		string str;
+		int k = 0;
+
+		in >> str;
+
+		string_split_smart(str, " +-", w, k);
+
+		for (int i = 0; i < k + 1; i++) {
+
+			int l = w[i].find("i");
+
+			if (l >= 0 && l < str.length()) {
+				w[i].erase(l, 1);
+
+				if (w[i].length() == 0 || w[i] == "-" || w[i] == "+") {
+					w[i] += "1";
+				}
+
+				double im = stod(w[i]);
+
+				comp.SetIm(im);
+			}
+			else {
+
+				if (w[i] != "") {
+					double real = stod(w[i]);
+
+					comp.SetReal(real);
+				}
+			}
+		}
+
 		return in;
 	}
 private:
-	double real;
-	double im;
+	int real;
+	int im;
+
+	double real_f;
+	double im_f;
 	double mod;
 };
 
 int main() {
 	localize();
 
-	Complex A(5, 3);
-	Complex B(5, -3);
+	Complex A = {1, 5};
+	Complex B(3, 2);
 	Complex C;
 
-	cout << "Введите комплексное число:" << endl;
-	cin >> C;
+	for (int i = 0; i < 5; i++) {
 
-	cout << "Complex A: " << A << endl;
-	cout << "Complex B: " << B << endl;
-	cout << "Complex C: " << C << endl;
-	cout << "Complex A + B: " << A + B << endl;
-	cout << "Complex A - B: " << A - B << endl;
-	cout << "Complex A * B: " << A * B << endl;
-	cout << "Complex B / C: " << B / C << endl;
+		cout << "Введите комплексное число:" << endl;
+		cout << "Complex A: ";
+		cin >> A;
+
+		cout << "Complex B: ";
+		cin >> B;
+
+		cout << "Complex C: ";
+		cin >> C;
+
+		cout << "\t\tВычисления..." << endl;
+
+		cout << "Сложение чисел:" << endl;
+
+		cout << "\t A + B: " << A + B << endl << "\t B + C: " << B + C << endl << "\t C + A: " << C + A << endl;
+
+		cout << "Умножение чисел:" << endl;
+
+		cout << "\t A * B: " << A * B << endl << "\t B * C: " << B * C << endl << "\t C * A: " << C * A << endl;
+
+		cout << "Деление чисел:" << endl;
+
+		cout << "\t A / B: " << A / B << endl << "\t B / C: " << B / C << endl << "\t C / A: " << C / A << endl;
+
+	}
+
 }
